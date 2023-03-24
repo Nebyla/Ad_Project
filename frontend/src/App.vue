ac394205af39f86eaba905a54af358a7e8439e33
Nebyla <95207952+Nebyla@users.noreply.github.com>
2023-03-24 11:04:21 +0300

    Подключение данных к grid

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -29,8 +29,8 @@ v-for="link in links"
   <v-app-bar app dark color="primary">
   <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
   <v-toolbar-title>
-      <router-link to="/" tag="span" class="pointer">Home</router-link>
-    </v-toolbar-title>
+    <router-link to="/" tag="span" class="pointer" >Home</router-link>
+  </v-toolbar-title>
 
   <v-spacer></v-spacer>
   <v-toolbar-items class="hidden-sm-and-down">
@@ -73,4 +73,12 @@ start
   }
   }
   </script>
+<style scoped>
+.pointer {
+color: white;
+text-decoration: none;
+cursor: pointer;
+}
+</style>
+
   
