f6575011ef8036845e9f874775796cafd2388c0a
Nebyla <95207952+Nebyla@users.noreply.github.com>
2023-04-07 19:34:52 +0300

    Подключение лоудера на экране логина

--- frontend/src/views/Auth/LoginView.vue
+++ frontend/src/views/Auth/LoginView.vue
@@ -29,9 +29,13 @@
 			</v-card-text>
 			<v-card-actions>
 			<v-spacer></v-spacer>
-			<v-btn color="primary" @click="onSubmit" :disabled="!valid">
-			Login
-			</v-btn>
+			<v-btn 
+color="primary"
+@click="onSubmit"
+:loading="loading"
+:disabled="!valid || loading">
+	Login
+</v-btn>
 			</v-card-actions>
 			</v-card>
 			</v-col>
@@ -49,7 +53,14 @@ valid: false,
 emailRules: [(v) => !!v || "E-mail is required",(v) => /.+@.+\..+/.test(v) || "E-mail must  be valid",],
 passwordRules: [(v) => !!v || "Name is required",(v) =>(v && v.length >= 6) ||"Password must be more or equel than 6 characters",],
 };
+
 },
+computed: {
+	loading() {
+		return this.$store.getters.loading
+	}
+},
+
 methods:{
 	onSubmit(){
 	if (this.$refs.form.validate()){
