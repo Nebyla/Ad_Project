ad882372b07a48b04709f658b62ff42a040b567e
Nebyla <95207952+Nebyla@users.noreply.github.com>
2023-04-22 17:08:12 +0300

    Создание store заказов

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,13 +2,15 @@ import { createStore } from 'vuex'
 import ads from './ads'
 import user from './user'
 import shared from './shared'
+import order from './order'
 
 
 const store = createStore({
 modules:{
 ads,
 user,
-shared
+shared,
+order
 }
 })
 
